5d64731292f960bca00bb926a665259e49f30ca8
ShaneWilmes <shanewilmes@yahoo.com>
2022-03-14 23:21:04 -0600

    output working

--- script.js
+++ script.js
@@ -1,107 +1,103 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "X", "Y", "Z"];
+const numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialChars = [" ", ",", "<", ".", ">", "/", "?", ";", ":", "'", "[", "{", "]", "}", "|", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",];
+
+
 // Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
+function writePassword(password) {
     var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
-
 };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 // generatePassword() generates random password 
 function generatePassword() {
 
-    var password = "";
-    var passwordLength = 8;
-    var passwordCharacterList = [];
+    // Getting password length and character types from user
+    const passwordLength = getPasswordLength();
+    const passwordCharacterList = getPasswordCharacterList() || [];
 
-    // Getting password lenth and character types from user
-    passwordLength = getPasswordLength();
-    passwordCharacterList = getPasswordCharacterList();
-
-
-    var index = 0;
-    for (let i = 0; i < passwordLength; i++) 
-    {
-        index = Math.floor(math.random() * passwordCharacterList.length);  //Confirming password is 8 characters and radomizing out put  
-        password += passwordCharacterList[index];
+    if (!isListValid(passwordCharacterList.length)) {
+        return
     }
 
-    return password; 
+    var password = "";
+
+    for (let index = 0; index < passwordLength; index++) {
+        password += passwordCharacterList[Math.floor(Math.random() * passwordCharacterList.length)];
+
+    }
+
+    writePassword(password);
+
 };
+
+function isListValid(listLength) {
+    if (listLength < 1) {
+        alert("You must select a character type")
+
+        return false;
+
+    } else {
+        return true;
+    }
+}
 
 //Getting password length from user
 function getPasswordLength() {
-    
-    var length = window.prompt("Password Length:  Please enter a number between 8 and 128.");
 
-    if (!lengthValidation(length)) {
+    var passwordLength = window.prompt("Password Length:  Please enter a number between 8 and 128.");
 
-        length = passwordLength();
+    if (passwordLength < 8) {
+        window.alert("Password must be at least 8 characters!");
+        getPasswordLength();
 
     }
 
-    return length;
+    if (passwordLength > 128) {
+        window.alert("Password must be less than 128 characters!")
+        getPasswordLength();
+    }
 
+    return passwordLength;
 };
 
-function characterTypes() {
-
-    var enterType = window.prompt("Would you like to include special caharacters?");
-
-    if (enterType) {
-        characterTypes();
-    
-    } 
-    
-};
 
 // Setting character types
 function getPasswordCharacterList() {
 
-    const lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "X", "Y", "Z"];
-    const numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    const specialChars = [" ", ",", "<", ".", ">", "/", "?", ";", ":", "'", "[", "{", "]", "}", "|", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",];
+    const passwordCharacterList = [];
 
-    //Sets to empty
-    var passwordCharacterList = [];
+    var lowercaseOption = confirm("Would you like lower case letters?");
 
-    var userInput = confirm("Would you like lower case letters?");
-    addToPasswordCharacterList(lowerCaseChars);
-
-    userInput = confirm("Would you like upper case letters?");
-    getPasswordCharacterList(upperCaseChars);
-
-    userInput = confirm("Would you like numbers?");
-    getPasswordCharacterList(numberChars);
-
-    userInput = confirm("Would you like special characters?");
-    getPasswordCharacterList(specialChars);    
-}
-
-
-function addToPasswordCharacterList(charsList); {
-    
-    if(userInput) {
-        
-        passwordCharacterList = passwordCharacterList.concat(charsList);
-    }    
-
+    if (lowercaseOption) {
+        passwordCharacterList.push.apply(passwordCharacterList, lowerCaseChars);
     }
 
 
+    var uppercaseOption = confirm("Would you like upper case letters?");
 
+    if (uppercaseOption) {
+        passwordCharacterList.push.apply(passwordCharacterList, upperCaseChars);
+    }
+    var numbersOption = confirm("Would you like numbers?")
 
+    if (numbersOption) {
+        passwordCharacterList.push.apply(passwordCharacterList, numberChars);
+    }
 
+    var specialOption = confirm("Would you like special characters?");
 
+    if (specialOption) {
+        passwordCharacterList.push.apply(passwordCharacterList, specialChars);
+    }
 
+    return passwordCharacterList
 
-
-
-
+}; 
